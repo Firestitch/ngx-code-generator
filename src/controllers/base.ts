1869494a94ea7087c821c4e6a486c03f95dc32ad
Eugene Kubesh <e.kubesh@wis.eu.com>
2018-09-05 16:15:02 +0300

    fixed commands

--- src/controllers/base.ts
+++ src/controllers/base.ts
@@ -32,41 +32,59 @@ export let index = (req: Request, res: Response) => {
     return;
   }
 
-  let command = `--name=${params.componentName} --module=${params.module.moduleName} --path=${params.module.modulePath}`;
+  let command = `\
+  --name=${params.componentName} \
+  --module=${params.module.moduleName} \
+  --path=${params.module.modulePath}`;
+
   let schema = '';
 
   switch (params.interfacePattern) {
     case ListCreationType.list: {
       schema = 'list';
-      // console.log(`ng g @firestitch/schematics:${schema} ${command}`);
-      exec(`ng g @firestitch/schematics:${schema} ${command}`, execHandler);
     } break;
+
     case ListCreationType.listCreateEditFull: {
       schema = 'list';
-      command += ' --create --edit';
-      exec(`ng g @firestitch/schematics:${schema} ${command}`, execHandler);
+      command += ' --mode=full';
     } break;
+
     case ListCreationType.listCreateEditDialog: {
       schema = 'list';
-      command += ` --dialog --singleName=${params.singularComponentName}`;
-      // console.log(`ng g @firestitch/schematics:${schema} ${command}`);
-      exec(`ng g @firestitch/schematics:${schema} ${command}`, execHandler);
-    } break;
-    case ListCreationType.CreateEditFull: {
-      schema = 'list-create-edit';
-      command = ` --parentName=${params.componentName} --module=${params.module.moduleFullPath} --secondLevel=true --path=${params.module.modulePath}/${params.componentName} --childName=create --childRoute=true`;
-      const command2 = ` --parentName=${params.componentName} --module=${params.module.moduleFullPath} --secondLevel=true --path=${params.module.modulePath}/${params.componentName} --childName=edit --childRoute=true`;
 
-      exec(`ng g @firestitch/schematics:${schema} ${command} && ng g @firestitch/schematics:${schema} ${command2}`, execHandler);//ng g @firestitch/schematics:list-create-edit --parentName=users --create --edit --module=/src/app/module1/module1.module.ts --project=test-app secondLevel=true --path=/src/app/module1/users --childName=create
+      command += `\
+      --mode=dialog \
+      --singleName=${params.singularComponentName}`;
+
+    } break;
+
+    case ListCreationType.CreateEditFull: {
+      schema = 'list-create';
+
+      command = `\
+      --mode='full'\
+      --parentName=${params.componentName} \
+      --module=${params.module.moduleFullPath} \
+      --secondLevel=true \
+      --path=${params.module.modulePath}/${params.componentName} \
+      --name=${params.singularComponentName}`;
     } break;
 
     case ListCreationType.CreateEditDialog: {
-      schema = 'list-create-edit-dialog';
-      command = ` --dialog --parentName=${params.componentName} --module=${params.module.moduleFullPath} --secondLevel=true --path=${params.module.modulePath}/${params.componentName} --singleName=${params.singularComponentName} --childName=${params.singularComponentName} --childRoute=true`;
-      // console.log(`ng g @firestitch/schematics:${schema} ${command}`);
-      exec(`ng g @firestitch/schematics:${schema} ${command}`, execHandler);
+      schema = 'list-create-dialog';
+
+      command = `\
+      --mode='dialog'
+      --parentName=${params.componentName} \
+      --module=${params.module.moduleFullPath} \
+      --secondLevel=true \
+      --path=${params.module.modulePath}/${params.componentName} \
+      --name=${params.singularComponentName}`;
+
     } break;
   }
+
+  exec(`ng g @firestitch/schematics:${schema} ${command}`, execHandler);
 };
 
 export let modulesList = (req: Request, res: Response) => {
