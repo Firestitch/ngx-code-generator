6cb67288d6fd5c2892d6c6d5cfb505fd580befb7
Eugene Kubesh <codbasters@gmail.com>
2024-12-12 21:20:43 +0100

    refactor: change resolver to ResolverFn

--- libs/schematics/src/generators/resolve/files/__name@dasherize__.resolve.ts
+++ libs/schematics/src/generators/resolve/files/__name@dasherize__.resolve.ts
@@ -1,31 +1,26 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 
 import { RouteSubject } from '@firestitch/core';
-
-import { Observable } from 'rxjs';
 
 import { toNumber } from 'lodash-es';
 
 import { <%= classify(serviceName) %> } from '<%= relativeServicePath %>';
 
+export const <%= name %>Resolver: ResolveFn<any>= (
+  route: ActivatedRouteSnapshot,
+  _: RouterStateSnapshot,
+) => {
+  const <%= camelize(serviceName)%> = inject(<%= classify(serviceName) %>);
+  const routeSubject = new RouteSubject();
 
-@Injectable()
-export class <%= classify(name) %>Resolve implements Resolve<any> {
-
-  constructor(private _<%= camelize(serviceName)%>: <%= classify(serviceName) %>) {}
-
-  public resolve(route: ActivatedRouteSnapshot): Observable<RouteSubject> {
-    const routeSubject = new RouteSubject();
-
-    if (!toNumber(route.params.id)) {
-      return routeSubject.next({});
-    }
-
-    const query = {};
-
-    return routeSubject
-      .observe(this._<%= camelize(serviceName) %>.get(route.params.id, query));
+  if (!toNumber(route.params.id)) {
+    return routeSubject.next({});
   }
 
-}
+  const query = {};
+
+  return routeSubject
+    .observe(this._<%= camelize(serviceName) %>.get(route.params.id, query));
+};
+
